807d7270eb895ab4ff7e18c9c62db258c1953f7d
Joachim Borup <joachim@borup.com>
2025-02-04 17:55:13 +0100

    Compose municipality graphs

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
+import time
+
+import networkx as nx
 import osmnx as ox
 
 
+def download_osm_graph(queries: list[str]) -> nx.MultiDiGraph:
+    def download_query(query: str) -> nx.MultiDiGraph:
+        start_time = time.time()
+        print(f"Retrieving graph for {query}...")
+        city_graph = ox.graph_from_place(
+            query, network_type="drive_service", simplify=True
+        )
+        print(f"Graph for {query} retrieved in {time.time() - start_time:.2f} seconds.")
+        return city_graph
+
+    return nx.compose_all([download_query(city) for city in queries])
+
+
 if __name__ == "__main__":
-    G = ox.graph_from_place(
-        "Capital Region of Denmark", network_type="drive_service", simplify=True
+    # G = download_osm_graph(["Capital Region of Denmark"])
+    G = download_osm_graph(
+        [
+            "Copenhagen Municipality, Denmark",
+            "Frederiksberg Municipality, Denmark",
+            "Tårnby Municipality, Denmark",
+            "Hvidovre Municipality, Denmark",
+            "Rødovre Municipality, Denmark",
+            "Gentofte Municipality, Denmark",
+            "Gladsaxe Municipality, Denmark",
+            "Herlev Municipality, Denmark",
+        ]
     )
     fig, ax = ox.plot.plot_graph(G, node_size=0, edge_linewidth=0.5, figsize=(10, 10))
