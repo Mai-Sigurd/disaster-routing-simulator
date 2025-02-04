de9130e016a844f20ad16941262d6ace4b55618d
Joachim Borup <joachim@borup.com>
2025-02-04 18:15:16 +0100

    Add logging

--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
-import time
+import logging
 
 import networkx as nx
 import osmnx as ox
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="{asctime} [{levelname}] {message}",
+    style="{",
+)
+
 
 def download_osm_graph(queries: list[str]) -> nx.MultiDiGraph:
     def download_query(query: str) -> nx.MultiDiGraph:
-        start_time = time.time()
-        print(f"Retrieving graph for {query}...")
+        logging.info(f"Downloading graph for {query}")
         city_graph = ox.graph_from_place(
             query, network_type="drive_service", simplify=True
         )
-        print(f"Graph for {query} retrieved in {time.time() - start_time:.2f} seconds.")
+        logging.info(f"Downloaded graph for {query}")
         return city_graph
 
     return nx.compose_all([download_query(city) for city in queries])
