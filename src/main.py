4cf03481d66a456f8668640cf8a344b6a5b6c8ee
Joachim Borup <73405008+JoachimBorup@users.noreply.github.com>
2025-02-07 13:16:14 +0100

    Add data loaders for OSM and GeoJSON data (#5)

--- src/main.py
+++ src/main.py
@@ -1,25 +1,15 @@
 import logging
 
-import networkx as nx
 import osmnx as ox
+
+from data_loader.danger_zones import load_danger_zone
+from data_loader.osm import download_osm_graph
 
 logging.basicConfig(
     level=logging.INFO,
     format="{asctime} [{levelname}] {message}",
     style="{",
 )
-
-
-def download_osm_graph(queries: list[str]) -> nx.MultiDiGraph:
-    def download_query(query: str) -> nx.MultiDiGraph:
-        logging.info(f"Downloading graph for {query}")
-        city_graph = ox.graph_from_place(
-            query, network_type="drive_service", simplify=True
-        )
-        logging.info(f"Downloaded graph for {query}")
-        return city_graph
-
-    return nx.compose_all([download_query(city) for city in queries])
 
 
 if __name__ == "__main__":
@@ -37,3 +27,5 @@ if __name__ == "__main__":
         ]
     )
     fig, ax = ox.plot.plot_graph(G, node_size=0, edge_linewidth=0.5, figsize=(10, 10))
+
+    danger_zones = load_danger_zone("dangerzone_amager.geojson")
