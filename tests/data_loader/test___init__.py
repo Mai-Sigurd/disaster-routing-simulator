725d42645a791d8c9e8b32101e8884c3d58fa074
Joachim Borup <73405008+JoachimBorup@users.noreply.github.com>
2025-02-11 14:43:20 +0100

    Add tests for data loaders (#8)

---
+++ tests/data_loader/test___init__.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+
+import pytest
+
+from data_loader import load_json
+
+
+def test_load_json_file_not_found(tmp_path: Path) -> None:
+    with pytest.raises(FileNotFoundError):
+        load_json(tmp_path / "test.json")
+
+
+def test_load_json_correct_loading(tmp_path: Path) -> None:
+    data = {"key": "value"}
+    file_path = tmp_path / "test.json"
+    file_path.write_text(json.dumps(data))
+
+    loaded_data = load_json(file_path)
+    assert loaded_data == data
