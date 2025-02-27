446d77889ea0aca87d15d8714d4e12f70a8ba948
Joachim Borup <joachim@borup.com>
2025-02-27 12:57:00 +0100

    Create basic Maven project

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import logging
+import shutil
+import subprocess
 
 from geopandas import GeoDataFrame
 
@@ -9,7 +11,7 @@ from data_loader.population.population import (
     get_origin_points,
     load_geojson,
 )
-from matsim_io import write_network, write_plans
+from matsim_io import MATSIM_DATA_DIR, write_network, write_plans
 from routes.fastestpath import fastest_path
 from routes.route import Route, create_route_objects
 from routes.shortestpath import path
@@ -23,6 +25,25 @@ logging.basicConfig(
 CPH_LOADED = True
 
 
+def run_matsim() -> None:
+    """
+    Run the MATSim executable with the config.xml file in the MATSIM_DATA_DIR.
+    """
+    matsim_output_dir = MATSIM_DATA_DIR / "output"
+    if matsim_output_dir.exists() and matsim_output_dir.is_dir():
+        shutil.rmtree(matsim_output_dir)
+
+    args = [
+        "java",
+        "-Xmx4G",
+        "-cp",
+        "matsim-2024.0.jar",
+        "org.matsim.run.RunMatsim",
+        MATSIM_DATA_DIR / "config.xml",
+    ]
+    subprocess.run(args, cwd=MATSIM_DATA_DIR)
+
+
 if __name__ == "__main__":
     if not CPH_LOADED:
         G = download_cph()
@@ -33,7 +54,7 @@ if __name__ == "__main__":
     population_data: GeoDataFrame = load_geojson("CPHpop.geojson")
 
     danger_zones: GeoDataFrame = load_danger_zone(
-        "mindre_del_af_amager.geojson", population_data.crs
+        "dangerzone_amager.geojson", population_data.crs
     )
     danger_zone_population: GeoDataFrame = distribute_population(
         danger_zones, population_data
@@ -56,3 +77,5 @@ if __name__ == "__main__":
 
     write_network(G, network_name="Copenhagen")
     write_plans(routes)
+
+    run_matsim()
