81a27ae3042d4885fb177208e80eb5099c13e3de
Joachim Borup <joachim@borup.com>
2025-02-27 15:49:39 +0100

    Run MATSim controller

--- simulator/src/main/java/org/disaster/routing/Main.java
+++ simulator/src/main/java/org/disaster/routing/Main.java
@@ -1,7 +1,29 @@
 package org.disaster.routing;
 
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File configFile = new File("../data/matsim/config.xml");
+        String absolutePath = configFile.getAbsolutePath();
+        if (!configFile.exists()) {
+            System.err.println("Config file not found at: " + absolutePath);
+            System.exit(1);
+        }
+
+        Config config = ConfigUtils.loadConfig(absolutePath);
+        config.network().setTimeVariantNetwork(true);
+
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+
+        Controler controler = new Controler(scenario);
+        controler.run();
     }
 }
