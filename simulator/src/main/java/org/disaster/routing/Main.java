8e2ad4dc579d39c06f84b9ce327327949cf80a25
Joachim Borup <joachim@borup.com>
2025-03-05 10:37:38 +0100

    Clean network before running simulation

--- simulator/src/main/java/org/disaster/routing/Main.java
+++ simulator/src/main/java/org/disaster/routing/Main.java
@@ -6,6 +6,7 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.controler.Controller;
 import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.network.algorithms.NetworkCleaner;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.simwrapper.SimWrapperModule;
 
@@ -16,19 +17,23 @@ public class Main {
         File configFile = new File("../data/matsim/config.xml");
         String absolutePath = configFile.getAbsolutePath();
         if (!configFile.exists()) {
-            System.err.println("Config file not found at: " + absolutePath);
+            System.err.printf("Config file not found: %s\n", absolutePath);
             System.exit(1);
         }
 
         Config config = ConfigUtils.loadConfig(absolutePath);
+
         File outputDir = new File(configFile.getParent(), config.controller().getOutputDirectory());
         config.controller().setOutputDirectory(outputDir.getAbsolutePath());
+
         config.network().setTimeVariantNetwork(true);
         config.controller().setOverwriteFileSetting(
-            OutputDirectoryHierarchy.OverwriteFileSetting.overwriteExistingFiles
+            OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists
         );
 
         Scenario scenario = ScenarioUtils.loadScenario(config);
+        new NetworkCleaner().run(scenario.getNetwork());
+
         Controller controller = new Controler(scenario);
         controller.addOverridingModule(new SimWrapperModule());
         controller.run();
