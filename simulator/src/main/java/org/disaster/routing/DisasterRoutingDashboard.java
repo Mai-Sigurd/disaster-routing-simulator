3d8344766e31e9a9a4c94d2ffe7af798412c73ee
amalieboegild <abso@itu.dk>
2025-04-04 12:49:54 +0200

    Make basic congestion dashboard work

--- simulator/src/main/java/org/disaster/routing/DisasterRoutingDashboard.java
+++ simulator/src/main/java/org/disaster/routing/DisasterRoutingDashboard.java
@@ -3,6 +3,7 @@ package org.disaster.routing;
 import org.disaster.routing.analysis.PeopleInSafetyXY;
 import org.disaster.routing.analysis.TripPurposeBy10Min;
 import org.disaster.routing.analysis.TripStatsDisaster;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.application.analysis.population.TripAnalysis;
 import org.matsim.application.analysis.traffic.TrafficAnalysis;
 import org.matsim.application.analysis.traffic.traveltime.TravelTimeComparison;
@@ -20,9 +21,18 @@ import tech.tablesaw.plotly.traces.BarTrace;
 import tech.tablesaw.plotly.traces.ScatterTrace;
 
 import java.util.List;
+import java.util.Set;
 
 public class DisasterRoutingDashboard implements Dashboard {
     private final String pythonDataPath = "../../../../../../../data/matsim/";
+    private final Set<String> modes;
+    public DisasterRoutingDashboard() {
+		this(Set.of(TransportMode.car));
+	}
+
+	public DisasterRoutingDashboard(Set<String> modes) {
+		this.modes = modes;
+	}
     @Override
     public void configure(Header header, Layout layout) {
         header.title = "Disaster Evacuation Routing Dashboard";
@@ -98,6 +108,63 @@ public class DisasterRoutingDashboard implements Dashboard {
 				);
 
 			}));
+
+
+            String[] args = new String[]{"--transport-modes", String.join(",", this.modes)};
+            layout.row("index_by_hour").el(Plotly.class, (viz, data) -> {
+
+				viz.title = "Network congestion index";
+				viz.description = "by hour";
+
+				Plotly.DataSet ds = viz.addDataset(data.compute(TrafficAnalysis.class, "traffic_stats_by_road_type_and_hour.csv", args));
+
+				viz.layout = tech.tablesaw.plotly.components.Layout.builder()
+					.yAxis(Axis.builder().title("Index").build())
+					.xAxis(Axis.builder().title("Hour").build())
+					.barMode(tech.tablesaw.plotly.components.Layout.BarMode.OVERLAY)
+					.build();
+
+				viz.addTrace(ScatterTrace.builder(Plotly.INPUT, Plotly.INPUT).mode(ScatterTrace.Mode.LINE).build(), ds.mapping()
+					.x("hour")
+					.y("congestion_index")
+					.name("road_type", ColorScheme.Spectral)
+				);
+			})
+			.el(Table.class, ((viz, data) -> {
+
+				viz.title = "Traffic stats per road type";
+				viz.description = "daily";
+
+				viz.dataset = data.compute(TrafficAnalysis.class, "traffic_stats_by_road_type_daily.csv", args);
+
+				viz.showAllRows = true;
+				viz.enableFilter = false;
+			}));
+
+		// TODO: Could be done per mode, by using the tab feature
+
+		layout.row("map").el(MapPlot.class, (viz, data) -> {
+
+			viz.title = "Traffic statistics";
+			viz.center = data.context().getCenter();
+			viz.zoom = data.context().mapZoomLevel;
+
+			viz.setShape(data.compute(CreateAvroNetwork.class, "network.avro"), "id");
+
+			viz.addDataset("traffic", data.compute(TrafficAnalysis.class, "traffic_stats_by_link_daily.csv"));
+
+			viz.display.lineColor.dataset = "traffic";
+			viz.display.lineColor.columnName = "avg_speed";
+			viz.display.lineColor.join = "link_id";
+			viz.display.lineColor.setColorRamp(ColorScheme.RdYlBu, 5, false);
+
+			viz.display.lineWidth.dataset = "traffic";
+			viz.display.lineWidth.columnName = "simulated_traffic_volume";
+			viz.display.lineWidth.scaleFactor = 20000d;
+			viz.display.lineWidth.join = "link_id";
+
+			viz.height = 12d;
+		});
     
     }
 
