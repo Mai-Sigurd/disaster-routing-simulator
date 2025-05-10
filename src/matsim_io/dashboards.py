f28f4b06e5ad9e3fee02add9587ce503ebee9257
Mai Sigurd <63573363+Mai-Sigurd@users.noreply.github.com>
2025-05-10 12:06:53 +0200

    remove unclassified from congestion index (#68)
    * remove unclassified from congestion index

--- src/matsim_io/dashboards.py
+++ src/matsim_io/dashboards.py
@@ -272,7 +272,7 @@ def create_comparison_dashboard(results: list[SimulationResult]) -> None:
             ),
         },
     }
-
+    remove_unclassified_from_trip_stats_by_road_type_and_hour_csv(results)
     dashboard_path = MATSIM_DATA_DIR / "dashboard-1.yaml"
     dashboard_path.write_text(yaml.dump(dashboard, sort_keys=False), encoding="utf-8")
 
@@ -561,3 +561,17 @@ def combine_csv_datasets(results: list[SimulationResult]) -> str:
     output_file = "analysis/people_in_safety.csv"
     merged_df.to_csv(MATSIM_DATA_DIR / output_file, index=False)
     return output_file
+
+
+def remove_unclassified_from_trip_stats_by_road_type_and_hour_csv(
+    results: list[SimulationResult],
+) -> None:
+    """
+    Remove the 'Unclassified' road type from the trip stat by road and hour CSV file.
+    :param results: List of SimulationResult objects containing the paths to the CSV files.
+    """
+    for result in results:
+        file_path = MATSIM_DATA_DIR / result.traffic_stats_csv_path
+        df = pd.read_csv(file_path)
+        df = df[df["road_type"] != "unclassified"]
+        df.to_csv(file_path, index=False)
